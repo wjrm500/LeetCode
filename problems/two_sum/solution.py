728bfa86e0ab36ca4eec19e8304d016ae8de1cc1
William May <48405095+wjrm500@users.noreply.github.com>
2021-11-16 01:20:58 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ### Two-line solution
         potSols = {target - num: i for i, num in enumerate(nums)}
-        for i, num in enumerate(nums):
-            if num in potSols and not i == potSols[num]:
-                return [i, potSols[num]]
+        return next([i, x] for i, num in enumerate(nums) if (x := potSols.get(num)) and i != x)
         
-            
+        ### Most memory-efficient solution
+        # potSols = {target - num: i for i, num in enumerate(nums)}
+        # for i, num in enumerate(nums):
+        #     if num in potSols and not i == potSols[num]:
+        #         return [i, potSols[num]]
